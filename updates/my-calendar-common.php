c1faf745d25aea5b2df9fabd2867d2101e65cb8c
joedolson <design@joedolson.com>
2016-02-10 14:44:13 -0600

    Switch to EDD license activation

--- updates/my-calendar-common.php
+++ updates/my-calendar-common.php
@@ -2,58 +2,67 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 // PHP functions common to all My Calendar pro packages
-function mcs_check_license( $key=false ) {
-	global $mcs_version;
-	if ( !$key ) {
-		return false;
-	} else {
-		//
-		define('MC_SUBMIT_PLUGIN_LICENSE_URL', "https://www.joedolson.com/wp-content/plugins/files/license.php" );
-		$response = wp_remote_post( MC_SUBMIT_PLUGIN_LICENSE_URL, 
-			array (
-				'user-agent' => 'WordPress/My Calendar Pro' . $mcs_version . '; ' . get_bloginfo( 'url' ), 
-				'body'=>array( 'key'=>$key, 'site'=>urlencode( home_url() ) ),
-				'timeout' => 30				
-			) );
-		if ( ! is_wp_error( $response ) || is_array( $response ) ) {
-			$data = $response['body'];
-			if ( !in_array( $data, array( 'false', 'inuse', 'true', 'unconfirmed' ) ) ) {
-				$data = @gzinflate(substr($response['body'],2));
-			}
-			return $data;
-		}
-		print_r( $response );
-	}
-	return false;
+function mcs_check_license() {
+	// listen for our activate button to be clicked
+	if( isset( $_POST['mcs_license_key'] ) ) {
+		// run a quick security check 
+	 	if( ! check_admin_referer( 'mcs-nonce', '_wpnonce' ) ) 	
+			return; // get out if we didn't click the Activate button
+		// retrieve the license from the database
+		$license = trim( $_POST[ 'mcs_license_key'] );
+		// data to send in our API request
+		$api_params = array( 
+			'edd_action'=> 'activate_license', 
+			'license' 	=> $license, 
+			'item_name' => urlencode( EDD_MCP_ITEM_NAME ), // the name of our product in EDD,
+			'url'       => home_url()
+		);
+		
+		// Call the custom API.
+		$response = wp_remote_post( EDD_MCP_STORE_URL, array(
+			'timeout'   => 15,
+			'sslverify' => false,
+			'body'      => $api_params
+		) );
+		// make sure the response came back okay
+		if ( is_wp_error( $response ) )
+			return false;
+		// decode the license data
+		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+		
+		// $license_data->license will be either "active" or "inactive"
+		return $license_data->license;
+	}	
 }
 
 function mcs_verify_key( $option='mcs_license_key', $lang='my-calendar-submissions', $name='My Calendar Pro') {
-	$key = $_POST[$option];
-	update_option( $option, $key );
-	if ( $key != '' ) {
-		$confirmation = mcs_check_license( $key );
+	$key = isset( $_POST[$option] ) ? $_POST[$option] : false;
+	if ( !$key ) {
+		return;
+	}
+	if ( $key !== '' ) {
+		$confirmation = mcs_check_license();
 	} else {
 		$confirmation = 'deleted';
 	}
+	update_option( $option, $key );
+	
 	$previously = get_option( $option.'_valid' );
 	update_option( $option.'_valid', $confirmation );
-	if ( $confirmation == 'false' ) {
-		$message = __("$name key not valid.", 'my-calendar-submissions' );
-	} else if ( $confirmation == 'inuse' ) {
-		$message = __("$name license key already registered.", 'my-calendar-submissions' );				
-	} else if ( $confirmation == 'unconfirmed' ) {
-		$message = __("Your payment for $name has not been confirmed.", 'my-calendar-submissions' );			
-	} else if ( $confirmation == 'true' ) {
-		if ( $previously == 'true' ) { 
+	if ( $confirmation == 'inactive' ) {
+		$message = __("$name key not valid.", 'my-calendar-submissions' );			
+	} else if ( $confirmation == 'active' ) {
+		if ( $previously == 'true' || $previously == 'active' ) { 
 		} else {
-			$message = __("$name key validated. Enjoy!", 'my-calendar-submissions' );
+			$message = __( "$name key activated. Enjoy!", 'my-calendar-submissions' );
 		}
 	} else if ( $confirmation == 'deleted' ) {
 		$message = __("You have deleted your $name license key.", 'my-calendar-submissions' );
 	} else {
-		$message = __("$name received an unexpected message from the license server. Try again in a bit.", 'my-calendar-submissions' );
+		$message = __("$name received an unexpected message from the license server. Try again soon!", 'my-calendar-submissions' );
 		delete_option( $option );
 	}
 	$message = ( $message != '' )?" $message ":$message; // just add a space
+	
 	return $message;
 }
