33e26fb6ea187fef4987239fee79a3d868b13008
Joe Dolson <design@joedolson.com>
2017-01-24 16:27:53 -0600

    Add reporting on license status.

--- my-calendar-license.php
+++ my-calendar-license.php
@@ -27,13 +27,38 @@ function mcs_license_tabs( $tabs ) {
 
 add_filter( 'mcs_settings_panels', 'mcs_license_key' );
 function mcs_license_key( $panels ) {
-
+	
 	$mcs_license_key       = get_option( 'mcs_license_key' );
+	mcs_recheck_license( $mcs_license_key );
 	$mcs_license_key_valid = ( get_option( 'mcs_license_key_valid' ) != '' ) ? " (" . get_option( 'mcs_license_key_valid' ) . ")" : '';
+	$mcs_license           = get_option( 'mcs_license_status' );
+	$output                = $expired = '';
+	
+	if ( !empty( $mcs_license ) ) {
+		foreach( $mcs_license as $test => $status ) {
+			$date = date_i18n( 'Y-m-d', $test );
+			$expires = ( $status->expires == 'lifetime' ) ? 'Lifetime' : date_i18n( 'Y-m-d', strtotime( $status->expires ) );
+			if ( $status->expires != 'lifetime' && strtotime( $status->expires ) < $test ) {
+				$expired = '<p>' . sprintf( 
+										__( 'Your license has expired. <a href="%s">Renew your license now!</a>', 'my-calendar-submissions' ), 
+										"https://www.joedolson.com/checkout/?edd_license_key=$mcs_license_key&download_id=5734" 
+									) . 
+							'</p>';
+			}
+			$license = $status->license;
+			$email   = $status->customer_email;
+			$output .= "<div class='mcs-license-status'>
+							<p>" . sprintf( __( 'Last Checked: %s', 'my-calendar-submissions' ), "<strong>$date</strong>" ) . "</p>
+							<p class='mcs-check-license'>" . sprintf( __( 'License expires on %s. License is %s. Customer email %s.', 'my-calendar-submissions' ), "<strong>$expires</strong>", "<strong>$license</strong>", "<strong>$email</strong>" ) . "</p>
+							$expired
+						</div>";
+		}
+	}
 	
 	$panels['license']['content'] = '
 		<h3>' . __( 'Activate Your License Key', 'my-calendar-submissions' ) . '</h3>
 		<div class="inside">
+			' . $output . '
 			<p class="license">
 			<label for="mcs_license_key">' . __('License key:','my-calendar-submissions') . $mcs_license_key_valid . '</label><br /><input type="text" name="mcs_license_key" id="mcs_license_key" class="widefat" size="60" value="' . esc_attr( trim( $mcs_license_key ) ) . '" />
 			</p>
--- updates/my-calendar-common.php
+++ updates/my-calendar-common.php
@@ -1,8 +1,36 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
+function mcs_recheck_license( $key ) {
+	$transient = get_transient( 'mcs_last_check' );
+	if ( $transient ) {
+		return;
+	} else {
+		$api_params = array(
+			'edd_action'=> 'check_license', 
+			'license' 	=> $key, 
+			'item_name' => urlencode( EDD_MCP_ITEM_NAME ), // the name of our product in EDD,
+			'url'       => home_url()
+		);
+		
+		// Call the custom API.
+		$response = wp_remote_post( EDD_MCP_STORE_URL, array(
+			'timeout'   => 15,
+			'sslverify' => false,
+			'body'      => $api_params
+		) );
+		// make sure the response came back okay
+		if ( is_wp_error( $response ) ) {
+			return false;
+		}
+		// decode the license data
+		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+		update_option( 'mcs_license_status', array( current_time( 'timestamp' ) => $license_data ) );
+		set_transient( 'mcs_last_check', true, DAY_IN_SECONDS );
+	}
+}
 // PHP functions common to all My Calendar pro packages
-function mcs_check_license() {
+function mcs_check_license( $output = 'status' ) {
 	// listen for our activate button to be clicked
 	if( isset( $_POST['mcs_license_key'] ) ) {
 		// run a quick security check 
@@ -11,7 +39,7 @@ function mcs_check_license() {
 		// retrieve the license from the database
 		$license = trim( $_POST[ 'mcs_license_key'] );
 		// data to send in our API request
-		$api_params = array( 
+		$api_params = array(
 			'edd_action'=> 'activate_license', 
 			'license' 	=> $license, 
 			'item_name' => urlencode( EDD_MCP_ITEM_NAME ), // the name of our product in EDD,
@@ -29,8 +57,9 @@ function mcs_check_license() {
 			return false;
 		// decode the license data
 		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+		update_option( 'mcs_license_status', array( current_time( 'timestamp' ) => $license_data ) );
+		// $license_data->license will be either "valid" or "?"
 		
-		// $license_data->license will be either "active" or "inactive"
 		return $license_data->license;
 	}	
 }
